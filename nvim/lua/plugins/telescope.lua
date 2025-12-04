7eb0bdb0d7fe8a871028021768670b98c0bda0ad
Matt Seabold <matt.seabold@gmail.com>
2025-12-03 23:02:13 -0500

    Add some basic telescope key mappings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,4 +1,11 @@
 return {
   'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
-  dependencies = { 'nvim-lua/plenary.nvim'}
+  dependencies = { 'nvim-lua/plenary.nvim'},
+  config = function(_, opts)
+    local telescope = require('telescope')
+    local builtin = require('telescope.builtin')
+
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find workspace files' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find workspace files' })
+  end
 }
