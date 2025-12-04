a515d1990b57055d39144042ff79782bd9484f7b
Matt Seabold <matt.seabold@gmail.com>
2025-12-03 22:31:38 -0500

    No italics in catppuccin

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,4 +1,11 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      no_italic = true
+    }
+  },
   'marko-cerovac/material.nvim'
 }
