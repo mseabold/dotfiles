eff5d1c2367e1cec4e2e9d4f8d717ef9eab79f52
Matt Seabold <matt.seabold@gmail.com>
2025-12-08 23:00:50 -0500

    Some key mappings and settings from old vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,17 @@
+-- Setup some basic keymaps
+
+-- Use jk/kj to exec insert
+vim.keymap.set('i', 'jk', '<esc>')
+vim.keymap.set('i', 'kj', '<esc>')
+
+-- C-h to remove search highlights
+vim.keymap.set({'n', 'i'}, '<C-h>', '<Cmd>nohl<CR>', { silent = true })
+
+-- Case insensitve searches for meeeee
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
 require("config.lazy")
 require("config.lsps")
 
 vim.cmd.colorscheme 'catppuccin-mocha'
-
