a4edfb50c0a87d0f49862e9b4eb7494667a1059e
Matt Seabold <matt.seabold@gmail.com>
2025-12-08 22:39:14 -0500

    More telescope mappings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,5 +7,12 @@ return {
 
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find workspace files' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find workspace files' })
+    vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Grep string in workspace' })
+    vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = 'Live grep' })
+
+    vim.keymap.set('n', '<leader>lr', builtin.lsp_references, { desc = 'Find LSP references' })
+    vim.keymap.set('n', '<leader>ld', builtin.lsp_definitions, { desc = 'Find LSP definitions' })
+    vim.keymap.set('n', '<leader>lx', builtin.diagnostics, { desc = 'LSP diagnostics' })
+    vim.keymap.set('n', '<leader>li', builtin.lsp_implementations, { desc = 'LSP implementations' })
   end
 }
